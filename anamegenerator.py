8f6244015766c3d0a3b86f92cbfa429e264ac582
Observer <80362048+68756d616e@users.noreply.github.com>
2022-10-19 19:17:26 +0100

    Add files via upload

---
+++ anamegenerator.py
@@ -0,0 +1,6 @@
+# Name generator
+# How it works is quite simple, the first and second input create the new name
+print("Hello welcome to a name generator")
+name1 = input("Please enter a first name \n")
+name2 = input("Please enter a second name \n")
+print("This is your new name " + name1 + " " + name2)
